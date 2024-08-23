ecebd8905fe00239d2a9ad48464a82dbd4b78c16
Andhre <mizandhre@gmail.com>
2024-08-22 21:57:09 -0500

    Extendiendo fecha del token

--- helpers/generateJWT.js
+++ helpers/generateJWT.js
@@ -4,7 +4,7 @@ const generateJWT = (uid, name) => {
 	const payload = { uid, name };
 
 	const token = jwt.sign(payload, process.env.JWT_SECRET, {
-		expiresIn: '2h',
+		expiresIn: '2d',
 	});
 
 	if (!token) return 'No se pudo generar el token';
